96b62ada88e35fa57d897adce95ed772fb4f370e
Pierre Teodoresco <p.teodoresco@gmail.com>
2025-12-24 18:15:43 +0100

    [CsVector] example

---
+++ examples/example_vector.c
@@ -0,0 +1,19 @@
+#include "cstash/result.h"
+#include "cstash/vector.h"
+
+#include <stdio.h>
+
+int main(void) {
+    CsVector* vector = vector_create(sizeof(int), 10);
+    int i = 0;
+    for (; i < 10; i++) {
+        if (vector_push(vector, &i) != CS_SUCCESS) {
+            return 1;
+        }
+    }
+    for (int j = 0; j < 10; j++) {
+        printf("%d ", *((int*)vector_get(vector, j)));
+    }
+    printf("\n");
+    return 0;
+}
